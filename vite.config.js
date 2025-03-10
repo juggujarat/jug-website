724f76acf7ea1311b7f9f8eaaca6c7b8504f3bf2
Daman <damansinghrajput247@gmail.com>
2025-03-10 14:56:16 +0530

    Update the vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()
+  ],
   base: "/",
+  build: {
+    outDir: 'dist'
+  },
   server: {
+    // port: 5177 ,
     host: "0.0.0.0",
     strictPort: true,
     cors: true,
   },
-  build: {
-    outDir: "dist"
-  },
-  resolve: {
-    alias: {
-      "@": "/src"
-    }
-  },
-  esbuild: {
-    jsxInject: `import React from 'react'`
-  },
-  define: {
-    "process.env": {},
-  },
-  preview: {
-    port: 4173,
-    host: true
-  }
 })
