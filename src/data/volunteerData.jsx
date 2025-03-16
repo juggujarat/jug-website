e439c9a53ded60ad4a535153b36c5472fe19e30a
Meet Patel <meetpatel.official1729@gmail.com>
2025-03-16 17:21:18 +0530

    Updated the Volunteer List

--- src/data/volunteerData.jsx
+++ src/data/volunteerData.jsx
@@ -4,6 +4,14 @@ const volunteer = [
     { id: 2, name: "Vikas Rajput", image: "/Volunteer/VikasSir.png", expertise: "Independent Java Consultant" },
     { id: 3, name: "Daman Singh Rajput", image: "/Volunteer/Daman.png", expertise: "Junior Developer at TechXplore" },
     { id: 4, name: "Rohan Kumar", image: "/Volunteer/Rohan.png", expertise: "Software Developer at Red Hat" },
+    { id: 5, name: "Bharat Ranpariya", image: "/Volunteer/BharatSir.png", expertise: "Tech Lead, Thompson Reuters" },
+    { id: 6, name: "Vikas Rajput", image: "/Volunteer/VikasSir.png", expertise: "Independent Java Consultant" },
+    { id: 7, name: "Daman Singh Rajput", image: "/Volunteer/Daman.png", expertise: "Junior Developer at TechXplore" },
+    { id: 8, name: "Rohan Kumar", image: "/Volunteer/Rohan.png", expertise: "Software Developer at Red Hat" },
+    { id: 9, name: "Bharat Ranpariya", image: "/Volunteer/BharatSir.png", expertise: "Tech Lead, Thompson Reuters" },
+    { id: 10, name: "Vikas Rajput", image: "/Volunteer/VikasSir.png", expertise: "Independent Java Consultant" },
+    { id: 11, name: "Daman Singh Rajput", image: "/Volunteer/Daman.png", expertise: "Junior Developer at TechXplore" },
+    { id: 12, name: "Rohan Kumar", image: "/Volunteer/Rohan.png", expertise: "Software Developer at Red Hat" },
   ];
   
   export default volunteer;
