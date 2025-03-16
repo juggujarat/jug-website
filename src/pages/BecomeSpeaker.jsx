b4aa70e6c61ae296a075636947de55794966df73
Meet Patel <meetpatel.official1729@gmail.com>
2025-03-16 16:49:04 +0530

    Changed Become Speaker Button Text and Style

--- src/pages/BecomeSpeaker.jsx
+++ src/pages/BecomeSpeaker.jsx
@@ -14,8 +14,11 @@ const BecomeSpeaker = () => {
               <div className='  ml-[60px] mt-8 sm:ml-[0px] sm:mt-8 sm:text-center'>
                 <h1 className="font-raleway font-medium text-[88px] leading-[103px] tracking-[1%] sm:text-[40px] sm:leading-[52px] sm:tracking-[1%] 
              text-black">Become a Speaker Guest</h1>
-                <button className="bg-[#FFFFFF] text-black px-7 py-5 rounded-2xl transition border border-black mt-2 sm:mt-6">
-                  Register as Speaker
+                <button className="relative px-7 py-5 font-medium text-black border-2 border-black rounded-lg bg-white overflow-hidden transition-all duration-300 group">
+                  <span className="absolute inset-0 bg-black scale-y-0 origin-bottom transition-transform duration-300 ease-in-out group-hover:scale-y-100"></span>
+                  <span className="relative z-10 text-black group-hover:text-white transition-colors duration-300">
+                    Submit Your Talk
+                  </span>
                 </button>
               </div>
             </div>
