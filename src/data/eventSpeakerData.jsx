bff7db1f0f8f2a45e2520782759c960bbe433a84
Daman <damansinghrajput247@gmail.com>
2025-03-17 23:13:44 +0530

    Rename the speaker name

--- src/data/eventSpeakerData.jsx
+++ src/data/eventSpeakerData.jsx
@@ -1,6 +1,6 @@
 const speaker = [
-    { id: 1, name: "Venkat Subramaniam", image: "/Experts/SpeakerVenkat.png", profession: "Developer Advocate at ", expertise: "JetBrains" },
-    { id: 2, name: "Vaibhav Choudhary", image: "/Experts/Vaibhav.png", profession: "JVM Engineer at ", expertise: "Salesforce" },
+    { id: 1, name: "Venkat Subramaniam", image: "/Experts/SpeakerVenkat.png", profession: "Founder of  ", expertise: "Agile Developer Inc." },
+    { id: 2, name: "Vaibhav Choudhary", image: "/Experts/Vaibhav.png", profession: "JVM Engineer at  ", expertise: "Salesforce" },
     { id: 3, name: "TBD", image: "/Experts/SpeakerShadow1.png", profession: "TBD ", expertise: "" },
     
     { id: 4, name: "TBD", image: "/Experts/SpeakerShadow2.png", profession: "TBD ", expertise: "" },
