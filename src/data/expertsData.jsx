e65dc141a2c3f71a33e3e4d6f1178fcb3e1b16f6
Vikas Rajput <vkhaas.rajput1993@gmail.com>
2025-03-21 00:28:58 +0530

    Add Gold Sponsor - Dataorb, correct volunteers & speakers details

--- src/data/expertsData.jsx
+++ src/data/expertsData.jsx
@@ -1,7 +1,7 @@
 
 const experts = [
     { id: 1, name: "Siva Reddy", image: "/Experts/Siva.png", profession: "Developer Advocate at ", expertise: "JetBrains" },
-    { id: 2, name: "Vikas Rajput", image: "/Experts/Vikas.png", profession: "Independent ", expertise: "Java Consultant" },
+    { id: 2, name: "Vikas Rajput", image: "/Experts/Vikas.png", profession: "Sr. Java Consultant at ", expertise: "TechXplore" },
     { id: 3, name: "Vaibhav Choudhary", image: "/Experts/Vaibhav.png", profession: "JVM Engineer at ", expertise: "Salesforce" },
     { id: 4, name: "Rohan Kumar", image: "/Experts/Rohan.png", profession: "Software Developer at ", expertise: "Red Hat" },
   ];
--- src/data/volunteerData.jsx
+++ src/data/volunteerData.jsx
@@ -1,13 +1,13 @@
 
 const team = [
     { id: 1, name: "Bharat Ranpariya", image: "/Team/Bharat.png", expertise: "Tech Lead at " , profession:"Thompson Reuters" },
-    { id: 2, name: "Vikas Rajput", image: "/Team/Vikas.png", expertise: "Independent " , profession:"Java Consultant"},
+    { id: 2, name: "Vikas Rajput", image: "/Team/Vikas.png", expertise: "Sr. Java Consultant at " , profession:"TechXplore"},
     { id: 3, name: "Dhaval Gajjar", image: "/Team/Dhaval.png", expertise: "System Architect at " , profession:"Staunchsys" },
     { id: 4, name: "Harshvardhan Parmar", image: "/Team/Harsh.png", expertise: "LFX'25 Mentee at ", profession:"Microcks" },
     { id: 5, name: "Vinay Rajput", image: "/Team/Vinay.png", expertise: "Visual Designer at " , profession:"Apexure"},
     { id: 6, name: "Kevin Gokani", image: "/Team/Kevin.png", expertise: "Java Developer at " , profession:"Qatar Airways"},
     { id: 7, name: "Shalin Sabuwala", image: "/Team/Shalin.png", expertise: "Java Developer at ", profession:"Sanctuary Tech" },
-    { id: 8, name: "Daman Singh Rajput", image: "/Team/Daman.png", expertise: "Java FullStack Developer at " , profession:"Techxplore"}
+    { id: 8, name: "Daman Singh Rajput", image: "/Team/Daman.png", expertise: "Java FullStack Developer at " , profession:"TechXplore"}
   
   ];
   
