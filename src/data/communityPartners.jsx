2b891ed9863ccb59988c0dd546105dc23f8f25ff
MeetCodeWizard <meetpatel.official1729@gmail.com>
2025-04-14 21:38:10 +0530

    Added Community Partners Section and Updated event Volunteer Issue

---
+++ src/data/communityPartners.jsx
@@ -0,0 +1,6 @@
+const partners = [
+    {id:1, name:"Docker Ahmedabad", image:"/Img/Docker_Ahmedabad.svg", link:"https://www.meetup.com/Docker-Ahmedabad/"},
+    {id:2, name:"Open Source Weekend Ahmedabad", image:"/Img/OSW_Ahmedabad.svg", link:"https://linktr.ee/osweekend"},
+]
+
+export default partners;
